8a97f244bc831c6d0141876b6bd7abbad5265349
Prateek Choudhary <prateekprakashchoudhary@gmail.com>
2025-04-02 01:10:30 +0530

    config file changes

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true, // Ensures proper routing
+  images: {
+    unoptimized: true, // Fixes potential image-related issues on Netlify
+  },
 };
 
 export default nextConfig;
